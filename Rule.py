0e98a9139c8aaea454214e25a1374370b62b9cc5
Tomáš Silber <silber.tomas@outlook.cz>
2021-10-05 16:18:26 +0200

    Support multiple tapes

--- Rule.py
+++ Rule.py
@@ -5,3 +5,7 @@ class Rule:
         self.next_state = next_state
         self.next_symbol = next_symbol
         self.operation = operation
+
+    def __str__(self) -> str:
+        return "" + self.current_state + " " + self.current_symbol + " " + self.next_state + " " + self.next_symbol + " " + self.operation
+
--- TurringMachine.py
+++ TurringMachine.py
@@ -1,5 +1,5 @@
 class TurringMachine:
-    def __init__(self, first_state, tape, rules, head=0, empty_symbol='#'):
+    def __init__(self, first_state, tape, rules, head=[0], empty_symbol='#'):
         self.tape = tape
         self.rules = rules
         self.head = head
@@ -9,44 +9,47 @@ class TurringMachine:
         self.number_of_steps = 0
         self.max_used_tape_length = len(self.tape)
 
-    def find_rule(self, symbol):
+    def find_rule(self, symbol, i):
         for e in self.rules:
-            if e.current_state.name == self.current_state.name and e.current_symbol == symbol:
-                return e
+            if e[i].current_state.name == self.current_state.name and e[i].current_symbol == symbol:
+                return e[i]
         raise Exception('Unable to find rule')
 
     def stylize_tape(self):
-        if self.tape[0] != self.empty_symbol:
-            self.tape = self.empty_symbol + self.tape
-            self.head = self.head + 1
+        for i in range(len(self.tape)):
+            if self.tape[i][0] != self.empty_symbol:
+                self.tape[i] = self.empty_symbol + self.tape[i]
+                self.head[i] = self.head[i] + 1
 
     def step(self):
         self.number_of_steps += 1
-        symbol = self.tape[self.head]
+        for i in range(len(self.head)):
+            symbol = self.tape[i][self.head[i]]
 
-        try:
-            rule = self.find_rule(symbol)
-        except Exception:
-            if (self.current_state.end is True):
+            try:
+                rule = self.find_rule(symbol, i)
+            except Exception:
+                if (self.current_state.end is True):
+                    self.finished = True
+                else:
+                    print('Unable to find rule')
+                return
+
+            self.tape[i] = self.tape[i][0:self.head[i]] + rule.next_symbol + self.tape[i][self.head[i] + 1:]
+            self.head[i] = self.head[i] + int(rule.operation)
+
+            # Make sure tape starts and ends with empty symbol
+            self.stylize_tape()
+
+            if self.head[i] < 0:
+                self.tape[i] = self.empty_symbol + self.tape[i]
+                self.head[i] = 0
+            elif self.head[i] > len(self.tape[i]):
+                self.tape[i] += self.empty_symbol
+
+            if self.current_state.end:
                 self.finished = True
-            else: print('Unable to find rule')
-            return
+            self.current_state = rule.next_state
 
-        self.tape = self.tape[0:self.head] + rule.next_symbol + self.tape[self.head+1:]
-        self.head = self.head + int(rule.operation)
-
-        # Make sure tape starts and ends with empty symbol
-        self.stylize_tape()
-
-        if self.head < 0:
-            self.tape = self.empty_symbol + self.tape
-            self.head = 0
-        elif self.head > len(self.tape):
-            self.tape += self.empty_symbol
-
-        if self.current_state.end:
-            self.finished = True
-        self.current_state = rule.next_state
-
-        if self.max_used_tape_length < self.head + 1:
-            self.max_used_tape_length = self.head + 1
+            if self.max_used_tape_length < self.head[i] + 1:
+                self.max_used_tape_length = self.head[i] + 1
